0cdd75e693c3450f3ff65b370a2b43bce84962f5
Federico Allegrini <federicoa@mondoconvenienza.net>
2020-09-22 14:54:33 +0200

    useEffect() & loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -6,6 +6,28 @@ import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // Run "after" and for "every" render cycle
+  // Without second argument acts like "componentDidUpdate", run after every component update
+  useEffect(
+    async () => {
+      const response = await fetch(
+        "https://react-hooks-update-9d013.firebaseio.com/ingredients.json"
+      );
+      const responseData = await response.json();
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    },
+    // With the second argument "[]" acts like "componentDidMount", run "only once" after the first render
+    []
+  );
 
   const addIngredientHandler = async (ingredient) => {
     const response = await fetch(
